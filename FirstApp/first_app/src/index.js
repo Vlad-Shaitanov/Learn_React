241eb025316ad0519abc51092941e55ea0b4cd3f
Vladislav <kelsenelenelvial@mail.ru>
2020-11-03 22:30:40 +0300

    first lesson

---
+++ FirstApp/first_app/src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+function Greatings(props) {
+	return (
+		<h1> Hello, {props.phrase}! My name is {props.name}</h1>
+	);
+}
+
+
+
+ReactDOM.render(
+	<React.StrictMode>
+		<Greatings phrase="World" name="Vlad" />
+	</React.StrictMode>,
+	document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
