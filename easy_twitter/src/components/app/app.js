a96d0314f2a40696211d0187fd12a6ad8cd455b6
Vladislav <kelsenelenelvial@mail.ru>
2020-12-02 20:32:00 +0300

    studying the types of component styling in react

--- easy_twitter/src/components/app/app.js
+++ easy_twitter/src/components/app/app.js
@@ -6,7 +6,19 @@ import PostStatusFilter from "../post-status-filter/index";
 import PostList from "../post-list/index";
 import PostAddForm from "../post-add-form/index";
 import "./app.css";
-import style from "./App.module.css";
+// import style from "./App.module.css";//Стилизация при помощи модулей
+import styled from "styled-components";
+
+//стилизация при помощи styled-components
+const AppBlock = styled.div`
+  margin: 0 auto;
+  max-width: 800px;
+`;
+
+//Пример наследования стилей от другого элемента + добавление новых
+// const StyledAppBlock = styled(AppBlock)`
+// 	background-color: grey;
+// `;
 
 const App = () => {//Компонент с приложением
 	const data = [
@@ -15,8 +27,8 @@ const App = () => {//Компонент с приложением
 		{ label: "I need a break...", important: false, id: "uio" }
 	];
 
-	return (
-		<div className={style.app}>
+	return (//className={style.app}
+		<AppBlock>
 			<AppHeader />
 			<div className="search-panel d-flex">
 				<SearchPanel />
@@ -24,7 +36,7 @@ const App = () => {//Компонент с приложением
 			</div>
 			<PostList posts={data} />
 			<PostAddForm />
-		</div>
+		</AppBlock>
 	);
 }
 
