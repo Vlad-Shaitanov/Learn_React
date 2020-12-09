f785fdb97c1be88a23dd7083b8ead2df7f62ca3f
Vladislav <kelsenelenelvial@mail.ru>
2020-12-09 15:53:38 +0300

    added toggle random character block

--- game_of_thrones_db/src/components/app/app.js
+++ game_of_thrones_db/src/components/app/app.js
@@ -1,38 +1,57 @@
-import React from "react";
-import { Col, Row, Container, Button } from "reactstrap";
+import React, { Component } from "react";
+import { Col, Row, Container } from "reactstrap";
 import Header from "../header/index.js";
 import RandomChar from "../randomChar/index.js";
 import ItemList from "../itemList/index.js";
 import CharDetails from "../charDetails/index.js";
+import ErrorMessage from "../errorMessage/index.js";
 
-const SwitchRandomChar = () => {
-	return
+import "./app.css";
+
+
+
+export default class App extends Component {
+	state = {
+		showRandomChar: true,//По умолчанию блок RandomChar виден
+		error: false
+	}
+	toggleRandomChar = () => {//Переключение видимости блока RandomChar по нажатию кнопки
+		this.setState((state) => {
+			return {
+				showRandomChar: !state.showRandomChar
+			}
+		});
+	}
+
+	render() {
+		if (this.state.error) {
+			return <ErrorMessage />
+		}
+		const char = this.state.showRandomChar ? <RandomChar /> : null;
+		return (
+			<>
+				<Container>
+					<Header></Header>
+				</Container>
+				<Container>
+					<Row>
+						<Col lg={{ size: 5, offset: 0 }}>
+							{char}
+							<button
+								className="toggle-btn"
+								onClick={this.toggleRandomChar}>Show/hide random character</button>
+						</Col>
+					</Row>
+					<Row>
+						<Col md="6">
+							<ItemList></ItemList>
+						</Col>
+						<Col md="6">
+							<CharDetails></CharDetails>
+						</Col>
+					</Row>
+				</Container>
+			</>
+		)
+	}
 }
-
-const App = () => {
-	return (
-		<>
-			<Container>
-				<Header></Header>
-			</Container>
-			<Container>
-				<Row>
-					<Col lg={{ size: 5, offset: 0 }}>
-						<RandomChar></RandomChar>
-						<Button color="primary" size="lg" block>Show/hide Random Character</Button>
-					</Col>
-				</Row>
-				<Row>
-					<Col md="6">
-						<ItemList></ItemList>
-					</Col>
-					<Col md="6">
-						<CharDetails></CharDetails>
-					</Col>
-				</Row>
-			</Container>
-		</>
-	)
-}
-
-export default App;
