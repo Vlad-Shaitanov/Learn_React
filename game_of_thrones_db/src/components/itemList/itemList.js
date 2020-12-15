a9257562cf679ac09a64784362445f2fe8a0a93b
Vladislav <kelsenelenelvial@mail.ru>
2020-12-15 15:06:46 +0300

    refactoring itemList.js with hooks

--- game_of_thrones_db/src/components/itemList/itemList.js
+++ game_of_thrones_db/src/components/itemList/itemList.js
@@ -1,58 +1,89 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Spinner from '../spinner/index.js';
-import PropTypes from "prop-types";
+// import PropTypes from "prop-types";
 import './itemList.css';
-export default class ItemList extends Component {
 
-	state = {
-		itemList: null,//Список персонажей
-	}
+//Component with hooks
+function ItemList({ getData, onItemSelected, renderItem }) {
+	const [itemList, updateList] = useState([]);
 
-	static defaultProps = {//Дефолтное значение пропса
-		onItemSelected: () => { }//Пустая функция
-	}
-	static propTypes = {
-		onItemSelected: PropTypes.func,//Проверяем что переданный пропс является функцией
-	}
-
-	componentDidMount() {
-		const { getData } = this.props;
-
+	useEffect(() => {
 		getData()
-			.then((itemList) => {//Список персов с определенной страницы
-				this.setState({ itemList })
+			.then((data) => {//Список персов с определенной страницы
+				updateList(data)
 			})
-	}
+	}, [])
 
-	renderItems(arr) {
+	function renderItems(arr) {
 		return arr.map((item) => {
 			const { id } = item;
-			const label = this.props.renderItem(item);//Имя перса
+			const label = renderItem(item);//Имя перса
 			return (
 				<li key={id}
 					className="list-group-item"
 					onClick={() => (
-						this.props.onItemSelected(id))}>
+						onItemSelected(id))}>
 					{label}
 				</li>
 			)
 		})
 	}
 
-	render() {
-		const { itemList } = this.state;
-
-		if (!itemList) {
-			return <Spinner />
-		}
-
-		const items = this.renderItems(itemList);
-
-		return (
-			<ul className="item-list list-group">
-				{items}
-			</ul>
-		);
+	if (!itemList) {
+		return <Spinner />
 	}
-}
 
+	const items = renderItems(itemList);
+
+	return (
+		<ul className="item-list list-group">
+			{items}
+		</ul>
+	);
+}
+export default ItemList;
+//Component without hooks
+// export default class ItemList extends Component {
+// 	state = {
+// 		itemList: null,//Список персонажей
+// 	}
+// 	static defaultProps = {//Дефолтное значение пропса
+// 		onItemSelected: () => { }//Пустая функция
+// 	}
+// 	static propTypes = {
+// 		onItemSelected: PropTypes.func,//Проверяем что переданный пропс является функцией
+// 	}
+// 	componentDidMount() {
+// 		const { getData } = this.props;
+// 		getData()
+// 			.then((itemList) => {//Список персов с определенной страницы
+// 				this.setState({ itemList })
+// 			})
+// 	}
+// 	renderItems(arr) {
+// 		return arr.map((item) => {
+// 			const { id } = item;
+// 			const label = this.props.renderItem(item);//Имя перса
+// 			return (
+// 				<li key={id}
+// 					className="list-group-item"
+// 					onClick={() => (
+// 						this.props.onItemSelected(id))}>
+// 					{label}
+// 				</li>
+// 			)
+// 		})
+// 	}
+// 	render() {
+// 		const { itemList } = this.state;
+// 		if (!itemList) {
+// 			return <Spinner />
+// 		}
+// 		const items = this.renderItems(itemList);
+// 		return (
+// 			<ul className="item-list list-group">
+// 				{items}
+// 			</ul>
+// 		);
+// 	}
+// }
