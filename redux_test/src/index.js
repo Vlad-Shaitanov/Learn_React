e4411d72b1b3bf8097e5a3b2b22f11f6b0ec630b
Vladislav <kelsenelenelvial@mail.ru>
2020-12-17 21:48:35 +0300

    work with Redux

--- redux_test/src/index.js
+++ redux_test/src/index.js
@@ -1,17 +1,60 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore } from "redux";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const reducer = (state = 0, action) => {
+	/*Функция принимает на вход 2 аргумента - state и action,
+	которое нужно произвести с состоянием*/
+	/*Функция reducer должна быть чистой, т.е. зависеть ТОЛЬКО от state и action*/
+	switch (action.type) {//Обработка типа действия
+		case "INC":
+			return state + 1;
+		case "DEC":
+			return state - 1;
+		case "RND":
+			return state + action.value;//action содержит значение value, которое передается внутрь как аргумент
+		default:
+			return state;
+	}
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const inc = () => {// action-creator(т.е. создает action)
+	return {
+		type: "INC"
+	}
+}
+const dec = () => {// action-creator(т.е. создает action)
+	return {
+		type: "DEC"
+	}
+}
+const rand = (value) => {// action-creator(т.е. создает action)
+	return {
+		type: "RND",
+		value
+	}
+}
+
+const store = createStore(reducer);//Переменная содержит в себе логику(reducer) и состояние
+
+document.querySelector("#incr").addEventListener("click", () => {
+	store.dispatch(inc());
+});
+document.querySelector("#decr").addEventListener("click", () => {
+	store.dispatch(dec());
+});
+document.querySelector("#rand").addEventListener("click", () => {
+	const value = Math.floor(Math.random() * 10);
+	store.dispatch(rand(value));
+});
+
+const update = () => {
+	document.querySelector("#counter").textContent = store.getState();
+};
+store.subscribe(update);
+
+// store.subscribe(() => {//Функция подписки на наш Store. Колбэк будет вызываться при каждом изменении state
+// 	console.log(store.getState());
+// });
+// store.dispatch({ type: "INC" });/*Метод нужен для запуска reducer
+// с состоянием из store и с тем Action, который мы передали*/
+// store.dispatch({ type: "INC" });
+// store.dispatch({ type: "INC" });
