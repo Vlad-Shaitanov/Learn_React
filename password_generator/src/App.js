6b139084cf0547192170e5974f28109908adb65b
Vladislav <kelsenelenelvial@mail.ru>
2021-01-24 21:48:18 +0300

    start application

---
+++ password_generator/src/App.js
@@ -0,0 +1,14 @@
+import Button from "./ui/button/index.js";
+import './App.css';
+
+function App() {
+	return (
+		<>
+			<Button type="button">
+				<span>Button</span>
+			</Button>
+		</>
+	);
+}
+
+export default App;
---
+++ password_generator/src/ui/button/button.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./button.module.css";
+
+const Button = ({ type, onClick, children }) => {
+	return (
+		<button
+			type={type}
+			className={styles["button"]}
+			onClick={onClick}
+		>{children}</button>
+	)
+};
+
+export default Button;
